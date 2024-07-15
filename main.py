f3c197558021c1b3232418797306cc352fbb98ff
MRbreaddz <mranos2009.100@gmail.com>
2024-07-15 03:04:06 -0700

    update

--- main.py
+++ main.py
@@ -6,10 +6,6 @@ from PySide2.QtCore import QUrl
 from PySide2.QtGui import QIcon
 from bs4 import BeautifulSoup
 import tempfile
-import glob
-import shutil
-import atexit
-
 
 html_file_contents = ''
 javascript_file_contents = ''
@@ -20,16 +16,6 @@ html_file_contents = html_file_contents.encode()
 with tempfile.NamedTemporaryFile(suffix='.html', delete=False) as temp_file:
     temp_file.write(html_file_contents)
     html_temp_file_name = temp_file.name
-
-files_type_list = ["*.png","*.jpeg","*.mp3","*.mp4","*.ico","*.webp","*.svg"]
-
-for i in files_type_list:
-    files = []
-    files = glob.glob("_internal/*"+str(i))
-    for a in files:
-        shutil.copy(a, r"C:\Users\bread\AppData\Local\Temp")
-    files = []
-
 
 
 app = QApplication(sys.argv)
@@ -58,17 +44,5 @@ web.load(QUrl.fromLocalFile(html_temp_file_name))
 web.setWindowTitle(str(title))
 web.setWindowIcon(QIcon(icon))
 web.show()
-
-def exit_handler():
-    for i in files_type_list:
-        files = []
-        files = glob.glob("_internal/*"+str(i))
-        for a in files:
-            os.remove(r"C:\Users\bread\AppData\Local\Temp/"+str(os.path.basename(a)))
-        files = []
-
-    os.remove(html_temp_file_name)
-
-atexit.register(exit_handler)
 
 sys.exit(app.exec_())
